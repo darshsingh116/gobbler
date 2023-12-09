69b12327daa8115cbb3a0c93c3d0b1d845cd53c0
darsh <darshsingh116@gmail.com>
2023-12-09 12:40:16 +0530

    made gobbler :)

--- src/Board.js
+++ src/Board.js
@@ -5,13 +5,13 @@ import { useDrag } from "react-dnd";
 import smallPieceYellow from './assets/small_yellow.png';
 import medPieceYellow from './assets/meduim_yellow.png';
 import largePieceYellow from './assets/large_yellow.png';
-import defaultImg from './assets/satisfied.jpg';
 import medPieceRed from './assets/meduim_red.png';
 import largePieceRed from './assets/large_red.png';
 import smallPieceRed from './assets/small_red.png';
 import medPiece from './assets/meduim.png';
 import largePiece from './assets/large.png';
 import smallPiece from './assets/small.png';
+import defaultPiece from './assets/default.png';
 
 const Board = () => {
 
@@ -27,9 +27,11 @@ const Board = () => {
     [0,0,0]
   ]);
   const [boardImg, setBoardImg] = useState([
-    defaultImg,defaultImg,defaultImg,defaultImg,defaultImg,defaultImg,defaultImg,defaultImg,defaultImg,defaultImg,
+    defaultPiece,defaultPiece,defaultPiece,defaultPiece,defaultPiece,defaultPiece,defaultPiece,defaultPiece,defaultPiece,defaultPiece,
   ]);
   // const [selected,setSelected] = useState(0);
+  const imgMap = [[defaultPiece,smallPieceYellow,medPieceYellow,largePieceYellow],[defaultPiece,smallPieceRed,medPieceRed,largePieceRed]];
+  
 
   const [gamePieces , setGamePieces] = useState([
     //s,m,l
@@ -205,7 +207,7 @@ const Board = () => {
 
     //updating the recieving cell
     index++;
-    const imgMap = [[defaultImg,smallPieceYellow,medPieceYellow,largePieceYellow],[defaultImg,smallPieceRed,medPieceRed,largePieceRed]];
+    
     boardImg[index] = imgMap[add-1][pieceSize+1];
     //console.log(imgMap[pieceSize]);
     setBoardImg(boardImg);
@@ -452,52 +454,40 @@ const Board = () => {
     updateGameState(x,y,add,remainder,index,myid);
     console.log("from " +myid+ " dropped to "+index);
   }
-
   if(won===0){
+    console.log(gamePieces[(playerTurn+1)%2]);
 
   return (<>
   <div className="bg">
       <h1>Player {(playerTurnDOM%2)===0?"Red":"Yellow"}'s turn</h1>
-    </div>
-  
+    </div> 
   <div className="board-container">
     <div className="board">
-    <div ref={drop1} className="board-cell" >
-    <img ref={drag1} src={boardImg[1]} width="50px"/>
-    </div>
-    <div ref={drop2} className="board-cell" >
-    <img ref={drag2} src={boardImg[2]} width="50px"/>
-    </div>
-    <div ref={drop3} className="board-cell" >
-    <img ref={drag3} src={boardImg[3]} width="50px"/>
-    </div>
-    <div ref={drop4} className="board-cell" ><img ref={drag4} src={boardImg[4]} width="50px"/></div>
-    <div ref={drop5} className="board-cell" ><img ref={drag5} src={boardImg[5]} width="50px"/></div>
-    <div ref={drop6} className="board-cell" ><img ref={drag6} src={boardImg[6]} width="50px"/></div>
-    <div ref={drop7} className="board-cell" ><img ref={drag7} src={boardImg[7]} width="50px"/></div>
-    <div ref={drop8} className="board-cell" ><img ref={drag8} src={boardImg[8]} width="50px"/></div>
-    <div ref={drop9} className="board-cell" ><img ref={drag9} src={boardImg[9]} width="50px"/></div>
-    </div>
-    
+    <div ref={drop1} className="board-cell" ><img className="cell-img" ref={drag1} src={boardImg[1]} /></div>
+    <div ref={drop2} className="board-cell" ><img className="cell-img" ref={drag2} src={boardImg[2]} /></div>
+    <div ref={drop3} className="board-cell" ><img className="cell-img" ref={drag3} src={boardImg[3]} /></div>
+    <div ref={drop4} className="board-cell" ><img className="cell-img" ref={drag4} src={boardImg[4]} /></div>
+    <div ref={drop5} className="board-cell" ><img className="cell-img" ref={drag5} src={boardImg[5]} /></div>
+    <div ref={drop6} className="board-cell" ><img className="cell-img" ref={drag6} src={boardImg[6]} /></div>
+    <div ref={drop7} className="board-cell" ><img className="cell-img" ref={drag7} src={boardImg[7]} /></div>
+    <div ref={drop8} className="board-cell" ><img className="cell-img" ref={drag8} src={boardImg[8]} /></div>
+    <div ref={drop9} className="board-cell" ><img className="cell-img" ref={drag9} src={boardImg[9]} /></div>
+    </div>   
   </div>
-  <div style={{ display: 'flex' }}>
-
-  
-<div>
-<img ref={drag01} src={smallPiece} width="100px" style={{margin:"20px"}}/>
-<div><span>Counter: 0</span></div>
+  <div className='span-div'> 
+<div className='image-container'>
+<img ref={drag01} src={imgMap[(playerTurnDOM+1)%2][1]} width="100px"/>
+<div><span>{gamePieces[(playerTurnDOM)%2][0]}</span></div>
 </div>
-<div>
-<img ref={drag02} src={medPiece} width="100px" style={{margin:"20px"}}/>
-<div><span>Counter: 0</span></div>
+<div className='image-container'>
+<img ref={drag02} src={imgMap[(playerTurnDOM+1)%2][2]} width="100px" />
+<div><span>{gamePieces[(playerTurnDOM)%2][1]}</span></div>
 </div>
-<div>
-<img ref={drag03} src={largePiece} width="100px" style={{margin:"20px"}}/>
-<div><span>Counter: 0</span></div>
+<div className='image-container'>
+<img ref={drag03} src={imgMap[(playerTurnDOM+1)%2][3]} width="100px" />
+<div><span>{gamePieces[(playerTurnDOM)%2][2]}</span></div>
 </div>
 </div>
-    
-
     </>
   );}
 
